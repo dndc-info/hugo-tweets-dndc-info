40d4f948a4373912b22a0429c775ab0be733721f
euonik <inouwe@gmail.com>
2023-12-16 12:10:40 +0900

    だいたい見た目を整えた

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html', './**/*.md'],
+    theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    // ...
+  ],
+}
